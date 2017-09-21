0b938a42e6a71be2206403521dfa62573c9d8794
logonrm <logonrm@fiap.com.br>
2017-09-20 21:56:45 -0300

    Criado a interface e classe DAL

---
+++ Fiap.Exemplo04.MVC/Controllers/JogadorController.cs
@@ -0,0 +1,40 @@
+﻿using Fiap.Exemplo04.MVC.Models;
+using Fiap.Exemplo04.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo04.MVC.Controllers
+{
+    public class JogadorController : Controller
+    {
+        private FutebolContext _context = new FutebolContext();
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_context.Jogadores.Include("Time").ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            //Buscar todos os times cadastrados
+            var lista = _context.Times.ToList();
+            //Passa os valores para o select da tela
+            ViewBag.times = new SelectList(lista, "TimeId", "Nome");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Jogador jogador)
+        {
+            _context.Jogadores.Add(jogador);
+            _context.SaveChanges();
+            TempData["msg"] = "Jogador Cadastrado!";
+            return RedirectToAction("Cadastrar");
+        }
+    }
+}
